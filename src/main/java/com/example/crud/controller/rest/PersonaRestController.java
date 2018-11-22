5bba835db596278ded6e1e5fb624883182cbe071
hbhuby <hbhuby@synopsis.ws>
2018-11-22 18:26:37 -0500

    reporte de controle al html con el tipo de dato model (con error)

--- src/main/java/com/example/crud/controller/rest/PersonaRestController.java
+++ src/main/java/com/example/crud/controller/rest/PersonaRestController.java
@@ -7,6 +7,7 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,20 +20,39 @@ import org.springframework.web.bind.annotation.RestController;
 import com.example.crud.beans.domain.Persona;
 import com.example.crud.beans.request.FileInformationRequest;
 import com.example.crud.beans.request.PersonaRequest;
-import com.example.crud.service.AutomovilService;
 import com.example.crud.service.PersonaService;
 
 @RestController
 @RequestMapping("/persona")
 public class PersonaRestController {
 
-	@Autowired
+    @Autowired
 	private PersonaService personaService;
+	
+	private Model model;
 
-	@GetMapping("/")
-	public ResponseEntity<List<Persona>> getPersonaList() {
+    @GetMapping("/list")
+	/**/public Model getPersonaList()
+	{
+	    Persona request = new Persona();
+	    personaService.getPersonaList();
+	    model.addAttribute("id",request.getId());
+	    model.addAttribute("primerNombre",request.getPrimerNombre());
+	    model.addAttribute("segundoNombre",request.getSegundoNombre());
+	    model.addAttribute("primerApellido",request.getPrimerApellido());
+	    model.addAttribute("segundoApellido",request.getSegundoApellido());
+	    model.addAttribute("tipoDoc",request.getTipoDoc());
+	    model.addAttribute("numeroDoc",request.getNumeroDoc());
+	    model.addAttribute("fechaNacimiento", request.getFechaNacimiento());
+	    model.addAttribute("estado", request.getEstado());
+	    model.addAttribute("fechaCreacion", request.getFechaCreacion());
+	    model.addAttribute("fechaActualizacion", request.getFechaActualizacion());
+	    return model;	
+	}/**/
+	/*public ResponseEntity<List<Persona>> getPersonaList() {
+	    
 		return new ResponseEntity<>(personaService.getPersonaList(), HttpStatus.OK);
-	}
+	}*/
 
 	@PostMapping("/")
 	public ResponseEntity<Map<String, String>> save(@RequestBody PersonaRequest request) {
@@ -42,7 +62,6 @@ public class PersonaRestController {
 	@PutMapping("/")
 	public ResponseEntity<Map<String, String>> modificar(@RequestBody PersonaRequest request){
 		return new ResponseEntity<>(personaService.modificar(request), HttpStatus.OK);
-		
 	}
 	
 	@GetMapping("/{id}")
@@ -59,5 +78,4 @@ public class PersonaRestController {
 	public ResponseEntity<Map<String, List<String>>> saveFromFile(@RequestBody FileInformationRequest request){
 		return new ResponseEntity<>(personaService.saveFromFile(request), HttpStatus.OK);
 	}
-	
 }
