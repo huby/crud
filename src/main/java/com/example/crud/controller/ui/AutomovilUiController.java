a280703507f9cf9547c2bc274fc202f17be4fe53
HECTOR HUBY <hectorhuby@MacBook-Pro-de-HECTOR.local>
2018-11-22 02:10:44 -0500

    layout html thymeleaf para mantenedores basicos

--- src/main/java/com/example/crud/controller/ui/AutomovilUiController.java
+++ src/main/java/com/example/crud/controller/ui/AutomovilUiController.java
@@ -3,19 +3,19 @@ package com.example.crud.controller.ui;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.example.crud.service.AutomovilService;
 
 @Controller
 @RequestMapping("/ui")
-public class AutomovilController {
+public class AutomovilUiController {
 
 	@Autowired
-	AutomovilService service;
+	private AutomovilService service;
 	
-	@RequestMapping(method = RequestMethod.GET, value = "/automovil")
+	@GetMapping(value = "/automovil")
 	public String index(Model model) {
 		model.addAttribute("automovilList", service.lista());
 		return "automovil/index";
--- src/main/java/com/example/crud/controller/ui/PersonaUiController.java
+++ src/main/java/com/example/crud/controller/ui/PersonaUiController.java
@@ -1,17 +1,16 @@
 package com.example.crud.controller.ui;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 @RequestMapping("/ui")
-public class PersonaUIController
+public class PersonaUiController
 {
-    //@RequestMapping(path = "/persona", method = RequestMethod.GET)
-    //public String ndex() {
-    //public String index() {
-    	//return null;
-        //return "index";
-    //}
+    @GetMapping(path = "/persona")
+    public String index() {
+        return "persona/index";
+    }
 }
